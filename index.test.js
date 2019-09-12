246ceddde4129b6e36ca2753581e5fd843f46c63
Brooks Carroll <bcarroll@middlebury.edu>
2019-09-12 16:07:53 -0400

    Second Commit

--- index.test.js
+++ index.test.js
@@ -17,20 +17,20 @@ describe('Determines age based on birthday', () => {
     // Set a fixed date
     Date.now = jest.fn(() => new Date('01 Jan 2018').valueOf());
   });
-
-  test('Returns 0 if birthday is today', () => {
+  // For tests, "today" means 01 Jan 2018
+  test('Returns 0 if birthday is 01 Jan 2018', () => {
     expect(birthday.howOld(new Date('01 Jan 2018'))).toBe(0);
   });
-  test('Returns 0 if birthday is today', () => {
+  test('Returns 1 if birthday is 01 Jan 2017', () => {
     expect(birthday.howOld(new Date('01 Jan 2017'))).toBe(1);
   });
-  test('Returns 0 if birthday is today', () => {
-    expect(birthday.howOld(new Date('05 Jan 2017'))).toBe(0);
+  test('Returns 0 if birthday is 05 Dec 2017', () => {
+    expect(birthday.howOld(new Date('05 Dec 2017'))).toBe(0);
   });
-  test('Returns 0 if birthday is today', () => {
+  test('Returns 1 if birthday is 05 Jan 2016', () => {
     expect(birthday.howOld(new Date('05 Jan 2016'))).toBe(1);
   });
-  test('Returns 0 if birthday is today', () => {
+  test('Returns 20 if birthday is 01 Jan 1998', () => {
     expect(birthday.howOld(new Date('01 Jan 1998'))).toBe(20);
   });
 });
