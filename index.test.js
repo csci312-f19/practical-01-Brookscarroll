f10e38ff70890c1baa56a87adf3aa8db3f448187
Brooks Carroll <bcarroll@middlebury.edu>
2019-09-12 15:35:00 -0400

    First Commit

---
+++ index.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-global-assign, no-underscore-dangle */
+const birthday = require('./index');
+
+describe('Determines age based on birthday', () => {
+  let _Date;
+  beforeAll(() => {
+    // Save original date module
+    _Date = Date;
+  });
+
+  afterAll(() => {
+    // Reset Date
+    Date = _Date;
+  });
+
+  beforeEach(() => {
+    // Set a fixed date
+    Date.now = jest.fn(() => new Date('01 Jan 2018').valueOf());
+  });
+
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('01 Jan 2018'))).toBe(0);
+  });
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('01 Jan 2017'))).toBe(1);
+  });
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('05 Jan 2017'))).toBe(0);
+  });
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('05 Jan 2016'))).toBe(1);
+  });
+  test('Returns 0 if birthday is today', () => {
+    expect(birthday.howOld(new Date('01 Jan 1998'))).toBe(20);
+  });
+});
